bc532d57acacb65b030518f3b5e3eb4405a9298f
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-09-14 21:58:04 +0430

    Start project

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = (props) => {
+    return (
+        <React.Fragment>
+            <header></header>
+            {props.children}
+        </React.Fragment>
+    )
+}
+
+export default Layout

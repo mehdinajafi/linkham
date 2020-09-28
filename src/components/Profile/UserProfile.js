a3c755ad33d4a7b5eafe1306e2da38f0f1cce000
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-09-29 00:06:13 +0330

    Show title and sub-headings in profile

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -1,7 +1,18 @@
 import React from "react"
 
-const UserProfile = () => {
-  return <div>UserProfile</div>
+const UserProfile = ({ userData }) => {
+  return (
+    <React.Fragment>
+      <div className="d-flex flex-column py-2">
+        <h4 className="text-dark text-center">
+          {userData.titleSubHeadings.title}
+        </h4>
+        <div className="text-secondary text-center">
+          {userData.titleSubHeadings.subHeadings}
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default UserProfile

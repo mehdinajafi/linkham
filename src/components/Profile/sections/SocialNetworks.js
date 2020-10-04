9a32dfcba026e8f47c36a324f50f5c7d7d695c02
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-10-04 23:57:27 +0330

    Improved display of social network links

--- src/components/Profile/sections/SocialNetworks.js
+++ src/components/Profile/sections/SocialNetworks.js
@@ -2,15 +2,22 @@ import React from "react"
 
 const SocialNetworks = ({ socialNetworks }) => {
   return (
-    <div className="d-flex flex-column justify-content-center">
+    <div className="d-flex flex-column">
       <h5 className="text-center text-muted">شبکه های اجتماعی</h5>
-      <div className="social-networks d-flex flex-column">
+      {/* If the number of links was even, display it in a row, otherwise a column */}
+      <div
+        className={`mb-2 social-networks ${
+          Object.keys(socialNetworks).length % 2 === 0
+            ? "social-networks-row"
+            : "social-networks-col"
+        }`}
+      >
         {/*-------------- Show links that the user has already entered --------------*/}
 
         {socialNetworks.telegram && (
           <a
             href={`https://t.me/${socialNetworks.telegram}`}
-            className="social-network-link social-networks-telegram d-flex justify-content-center align-items p-3 mb-2"
+            className="social-network-link social-networks-telegram d-flex justify-content-center align-items-center rounded p-3 w-100"
             target="_blank"
           >
             <img
@@ -26,7 +33,7 @@ const SocialNetworks = ({ socialNetworks }) => {
         {socialNetworks.instagram && (
           <a
             href={`https://instagram.com/${socialNetworks.instagram}`}
-            className="social-network-link social-networks-instagram d-flex justify-content-center align-items p-3 mb-2"
+            className="social-network-link social-networks-instagram d-flex justify-content-center align-items-center rounded p-3 w-100"
             target="_blank"
           >
             <img
@@ -41,7 +48,7 @@ const SocialNetworks = ({ socialNetworks }) => {
         {socialNetworks.github && (
           <a
             href={`https://github.com/${socialNetworks.github}`}
-            className="social-network-link social-networks-github d-flex justify-content-center align-items p-3 mb-2"
+            className="social-network-link social-networks-github d-flex justify-content-center align-items-center rounded p-3 w-100"
             target="_blank"
           >
             <img
@@ -56,7 +63,7 @@ const SocialNetworks = ({ socialNetworks }) => {
         {socialNetworks.linkedin && (
           <a
             href={`https://linkedin.com/in/${socialNetworks.linkedin}`}
-            className="social-network-link social-networks-linkedin d-flex justify-content-center align-items p-3 mb-2"
+            className="social-network-link social-networks-linkedin d-flex justify-content-center align-items-center rounded p-3 w-100"
             target="_blank"
           >
             <img
